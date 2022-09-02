a2ac0e61cc72e8a6e73ce634f3749e6554784984
irmansyah38 <irmansya146@gmail.com>
2022-09-02 09:08:47 +0700

    membuat portopolio ,menambahkan fitur click langsung ganti foto dan file javascript

---
+++ java.js
@@ -0,0 +1,34 @@
+
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+var img5 = document.getElementById("img5");
+var img6 = document.getElementById("img6");
+
+
+img1.addEventListener('click', function () {
+    var img = document.getElementById("img").src='img/ace.jpg';
+})
+
+img2.addEventListener('click', function () {
+    var img = document.getElementById("img").src='img/dragon.jpg';
+})
+
+img3.addEventListener('click', function () {
+    var img = document.getElementById("img").src='img/garp.jpg';
+})
+
+img4.addEventListener('click', function () {
+    var img = document.getElementById("img").src='img/zoro.jpg';
+})
+
+img5.addEventListener('click', function () {
+    var img = document.getElementById("img").src='img/ussop.jpg';
+})
+
+img6.addEventListener('click', function () {
+    var img = document.getElementById("img").src='img/luffy.jpg';
+})
+
+
